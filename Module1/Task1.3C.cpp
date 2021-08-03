42fa34169fe44ecbee21d2a2e014569ceb17d4c9
Rakyan Adhikara <rkyn.aa@gmail.com>
2021-08-04 01:40:23 +1000

    Task 1.3C Updated

--- Module1/Task1.3C.cpp
+++ Module1/Task1.3C.cpp
@@ -1,53 +1,50 @@
 // C++ code
 //
-const byte pirPin = 2;
-const byte echoPin = 13;    // Ultrasonic Echo Pin
-const byte trigPin = A0;    // Ultrasonic Trigger Pin
+const byte pirPin1 = 2;
+const byte pirPin2 = 3;
 const byte ledRedPin = 7;   // LED RED Pin
 const byte ledGreenPin = 6; // LED GREEN Pin
-volatile byte ledState = HIGH;
-volatile byte pirState = LOW;
+volatile byte ledRedState = LOW;
+volatile byte ledGreenState = LOW;
+volatile byte pirStateRed = LOW;
+volatile byte pirStateGreen = LOW;
 
 void setup()
 {
-    Serial.begin(9600);            // Starting Serial Terminal
-    pinMode(pirPin, INPUT_PULLUP); // Set the pin for PIR
-    pinMode(trigPin, OUTPUT);      // Set the pin for trigger
-    pinMode(echoPin, INPUT);       // Set the pin for echo
-    pinMode(ledRedPin, OUTPUT);    // Set the pin for LED RED
-    pinMode(ledGreenPin, OUTPUT);  // Set the pin for LED GREEN
-    attachInterrupt(digitalPinToInterrupt(pirPin), turnLED, RISING);
+    Serial.begin(9600);             // Starting Serial Terminal
+    pinMode(pirPin1, INPUT_PULLUP); // Set the pin for PIR1
+    pinMode(pirPin2, INPUT_PULLUP); // Set the pin for PIR2
+    pinMode(ledRedPin, OUTPUT);     // Set the pin for LED RED
+    pinMode(ledGreenPin, OUTPUT);   // Set the pin for LED GREEN
+    attachInterrupt(digitalPinToInterrupt(pirPin1), turnLEDRed, RISING);
+    attachInterrupt(digitalPinToInterrupt(pirPin2), turnLEDGreen, RISING);
 }
 
 void loop()
 {
-    long duration, distance; // Distance in cm
-
-    // Calculating distance
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = duration / 29 / 2;
-
     // Detecting motion to trigger interruption
-    pirState = digitalRead(pirPin);
+    pirStateRed = digitalRead(pirPin1);
+    pirStateGreen = digitalRead(pirPin2);
 
-    //Serial.print("No motion");
-    Serial.print(distance);
-    Serial.print("cm");
+    Serial.print("No motion. . . .");
     Serial.println();
-    digitalWrite(ledRedPin, ledState);
-    digitalWrite(ledGreenPin, !ledState);
+    digitalWrite(ledRedPin, ledRedState);
+    digitalWrite(ledGreenPin, ledGreenState);
     delay(500);
 }
 
-void turnLED()
+void turnLEDRed()
 {
-    ledState = !ledState;
-    Serial.print("Motion Detected! Interrupted!");
+    ledRedState = !ledRedState;
+    Serial.print("Motion Detected! RED LED!");
+    Serial.println();
+    delay(2000);
+}
+
+void turnLEDGreen()
+{
+    ledGreenState = !ledGreenState;
+    Serial.print("Motion Detected! GREEN LED!");
     Serial.println();
     delay(2000);
 }
--- Module1/Task1.3C.ino
+++ Module1/Task1.3C.ino
@@ -1,53 +1,50 @@
 // C++ code
 //
-const int pirPin = 2; // PIR Pin
-const int echoPin = 13; // Ultrasonic Echo Pin
-const int trigPin = 12; // Ultrasonic Trigger Pin
-const int ledRedPin = 7; // LED RED Pin
-const int ledGreenPin = 6; // LED GREEN Pin
-volatile byte ledState = HIGH;
-volatile byte pirState = LOW;
+const byte pirPin1 = 2;
+const byte pirPin2 = 3;
+const byte ledRedPin = 7; // LED RED Pin
+const byte ledGreenPin = 6; // LED GREEN Pin
+volatile byte ledRedState = LOW;
+volatile byte ledGreenState = LOW;
+volatile byte pirStateRed = LOW;
+volatile byte pirStateGreen = LOW;
 
 void setup()
 {
     Serial.begin(9600);            // Starting Serial Terminal
-    pinMode(pirPin, INPUT_PULLUP); // Set the pin for PIR
-  	pinMode(trigPin, OUTPUT); // Set the pin for trigger
-  	pinMode(echoPin, INPUT); // Set the pin for echo
+    pinMode(pirPin1, INPUT_PULLUP); // Set the pin for PIR1
+    pinMode(pirPin2, INPUT_PULLUP); // Set the pin for PIR2
     pinMode(ledRedPin, OUTPUT);       // Set the pin for LED RED
     pinMode(ledGreenPin, OUTPUT);       // Set the pin for LED GREEN
-    attachInterrupt(digitalPinToInterrupt(pirPin), turnLED, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(pirPin1), turnLEDRed, RISING);
+    attachInterrupt(digitalPinToInterrupt(pirPin2), turnLEDGreen, RISING);
 }
 
 void loop()
 {
-    long duration, distance; // Distance in cm
+    // Detecting motion to trigger interruption
+    pirStateRed = digitalRead(pirPin1);
+    pirStateGreen = digitalRead(pirPin2);
   
-  	// Calculating distance
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = duration / 29 / 2;
-  
-  	// Detecting motion to trigger interruption
-    pirState = digitalRead(pirPin);
-  
-    //Serial.print("No motion");
-  	Serial.print(distance);
-  	Serial.print("cm");
+  	Serial.print("No motion. . . .");
     Serial.println();
-    digitalWrite(ledRedPin, ledState);
-    digitalWrite(ledGreenPin, !ledState);
+    digitalWrite(ledRedPin, ledRedState);
+    digitalWrite(ledGreenPin, ledGreenState);
     delay(500);
 }
 
-void turnLED()
+void turnLEDRed()
 {
-    ledState = !ledState;
-    Serial.print("Interrupted!");
+  	ledRedState = !ledRedState;
+    Serial.print("Motion Detected! RED LED!");
     Serial.println();
-    delay(3000);
+    delay(2000);
+}
+
+void turnLEDGreen()
+{
+  	ledGreenState = !ledGreenState;
+    Serial.print("Motion Detected! GREEN LED!");
+    Serial.println();
+    delay(2000);
 }
